af932168093d93a31652b0d0ac3664b350d61e53
Найдолинский Дмитрий <naidv88@gmail.com>
2017-04-27 18:29:22 +0500

    updatedb.sh

--- updatedb.sh
+++ updatedb.sh
@@ -12,22 +12,10 @@ then
 	wget ${UP} 
 	if [ $? = "0" ]
 	then
-		rm -r ~/$1/*
+		rm -r $1/*
 	fi
-	FIND_NAME=10
-	while [ -z "$archive" ]
-	do
-		archive=$(echo ${UP}|cut -d / -f ${FIND_NAME})
-		((FIND_NAME--))
-	done
-		end=`echo $archive|cut -d . --complement  -f -1`
-		case "$end" in 
-			"gz") UNPACK=gunzip;;
-			"tar.gz") UNPACK="tar zxvf $archive -C $1";;
-			"tar.xz") UNPACK="tar xvfp $archive -C $1";;
-			"tar.bzip2") UNPACK="tar xvfj $archive -C $1";;
-		esac
-		${UNPACK}
+	echo "extracting ..."
+	tar zxf RFC-all.tar.gz $1
 fi
 ./rebuild "$1/rfc-index.txt" # list rfc docs
 if [ -f index ]
